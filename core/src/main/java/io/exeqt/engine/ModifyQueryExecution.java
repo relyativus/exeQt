8231a0907368efc98591a68771ecf8fce6d94528
relyativus <relyativus@gmail.com>
2018-09-06 19:32:00 +0300

    Added static import for Flow.Publisher in ModifyQueryExecution

--- core/src/main/java/io/exeqt/engine/ModifyQueryExecution.java
+++ core/src/main/java/io/exeqt/engine/ModifyQueryExecution.java
@@ -2,20 +2,20 @@ package io.exeqt.engine;
 
 import io.exeqt.result.ModifyResult;
 
-import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Publisher;
 
 /**
  * @author anatolii vakaliuk
  */
 public interface ModifyQueryExecution {
-    default Flow.Publisher<ModifyResult> modify(final String query, Object[] arguments) {
+    default Publisher<ModifyResult> modify(final String query, Object[] arguments) {
         return modify(Query.builder().setSql(query).setArguments(arguments).build());
     }
 
-    default Flow.Publisher<ModifyResult> modify(final String query) {
+    default Publisher<ModifyResult> modify(final String query) {
         return modify(query, null);
     }
 
-    Flow.Publisher<ModifyResult> modify(final Query query);
+    Publisher<ModifyResult> modify(final Query query);
 
 }
