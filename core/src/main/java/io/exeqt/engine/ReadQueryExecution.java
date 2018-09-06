3b72e874e8acf72eb12b3df4600cda067d623ac9
relyativus <relyativus@gmail.com>
2018-09-07 00:29:05 +0300

    Added static import for ReadQueryExecution

--- core/src/main/java/io/exeqt/engine/ReadQueryExecution.java
+++ core/src/main/java/io/exeqt/engine/ReadQueryExecution.java
@@ -2,19 +2,19 @@ package io.exeqt.engine;
 
 import io.exeqt.result.Row;
 
-import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Publisher;
 
 /**
  * @author anatolii vakaliuk
  */
 public interface ReadQueryExecution {
-    Flow.Publisher<Row> read(final Query query);
+    Publisher<Row> read(final Query query);
 
-    default Flow.Publisher<Row> read(final String query) {
+    default Publisher<Row> read(final String query) {
         return read(query, null);
     }
 
-    default Flow.Publisher<Row> read(final String query, Object[] arguments) {
+    default Publisher<Row> read(final String query, Object[] arguments) {
         return read(Query.builder().setSql(query).setArguments(arguments).build());
     }
 
